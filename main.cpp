e0a4b35f8ab83b9ffe6889493d5ad9d9c142721f
LaurentBerger <laurent.berger@univ-lemans.fr>
2016-01-13 20:48:53 +0100

    test

--- main.cpp
+++ main.cpp
@@ -4,12 +4,13 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    std::vector<double> pb={10,12};
-    IIRFilter f("butterworth",2,30,pb);
-    for (int i = 0; i<f.a.size();i++)
-        cout << f.a[i] << "\t";
-        cout <<  "\n";
+    std::vector<double> pb={5,10};
+    IIRFilter f("butterworth",4,30,pb);
+    cout <<  "Numerator = ";
     for (int i = 0; i<f.b.size();i++)
         cout << f.b[i] << "\t";
-        cout <<  "\n";
+    cout <<  "\n                  -----------------------\nDenominator = ";
+    for (int i = 0; i<f.a.size();i++)
+        cout << f.a[i] << "\t";
+    cout <<  "\n  ";
 }
