ecc5919dcc6cef7941e8a6e2e869e7b71c1c815e
LaurentBerger <laurent.berger@univ-lemans.fr>
2016-01-14 22:13:20 +0100

    Pyramide gaussienne et laplacienne

--- main.cpp
+++ main.cpp
@@ -1,9 +1,64 @@
+#include "opencv2/opencv.hpp"
 #include "IIRFilter.hpp"
 #include<iostream>
+
 using namespace std;
+using namespace cv;
+
+
+class PyramideGaussienne {
+vector<Mat> pyramide;
+public :
+    PyramideGaussienne(Mat );
+    vector<Mat> &get(){return pyramide;};
+
+};
+
+class PyramideLaplacienne {
+vector<Mat> pyramide;
+public :
+    PyramideLaplacienne(vector<Mat> &);
+    vector<Mat> &get(){return pyramide;};
+
+};
+
+
+PyramideGaussienne::PyramideGaussienne(Mat m)
+{
+    Mat x=m;
+    Mat y;
+    pyramide.push_back(x);
+    while (x.rows >= 4 && x.cols > 4)
+    {
+        pyrDown(x,y);
+        pyramide.push_back(y);
+        x=y;
+    }
+
+}
+
+PyramideLaplacienne::PyramideLaplacienne(vector<Mat> &m)
+{
+    Mat y;
+    for (int i = 1; i<m.size();i++)
+    {   
+        Mat tmp1,tmp2;
+        pyrUp(m[i],tmp1);
+        subtract(m[i-1],tmp1,tmp2,noArray(),CV_32F);
+        pyramide.push_back(tmp2);
+    }
+
+}
+
 
 int main(int argc, char **argv)
 {
+    Mat m = imread("f:/lib/opencv/samples/data/lena.jpg");
+
+    PyramideGaussienne pg(m);
+    PyramideLaplacienne pl(pg.get());
+
+
     std::vector<double> pb={5,10};
     IIRFilter f("butterworth",4,30,pb);
     cout <<  "Numerator = ";
