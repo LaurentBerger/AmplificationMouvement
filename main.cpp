9f8c127f4a0f9df2ea3d837ff1b75ffede92f790
LaurentBerger <laurent.berger@univ-lemans.fr>
2016-02-10 13:40:13 +0100

    A debugger

--- main.cpp
+++ main.cpp
@@ -80,6 +80,7 @@ public :
 class PyramideLaplacienne:public Pyramide {
 public :
     PyramideLaplacienne(vector<Mat> &);
+    Mat Collpase(Pyramide &gauss);
     vector<Mat> &get(){return pyr;};
 
 };
@@ -121,6 +122,18 @@ PyramideLaplacienne::PyramideLaplacienne(vector<Mat> &m)
         pyr.push_back(tmp2);
     }
 
+}
+
+Mat PyramideLaplacienne::Collpase(Pyramide &gauss)
+{
+    Mat y;
+    gauss[ pyr.size()].convertTo(y,CV_32F);
+    for (int i = pyr.size()-1; i>=0;i--)
+    {   
+        pyrUp(y,y);
+        add(y,pyr[i],y);
+    }
+    return y;
 }
 
 PyramideRiesz::PyramideRiesz(vector<Mat> &m)
@@ -257,7 +270,10 @@ Mat IIRtemporalFilter(IIRFilter &f, Mat phase, Mat *r0, Mat *r1)
     Mat tmp;
     tmp = f.b[0] * phase + (*r0);
     *r0 = f.b[1] * phase + (*r1) - f.a[1]*tmp;
-    *r1 = f.b[2] * phase  - f.a[2]*tmp;
+    if (f.b.size()==3)
+        *r1 = f.b[2] * phase  - f.a[2]*tmp;
+    else
+        *r1 = 0;
     return tmp;
 }
 
@@ -296,12 +312,12 @@ Mat PhaseShiftCoefficientRealPart(Mat laplevel, Mat rieszXLevel, Mat rieszYLevel
 
 int main(int argc, char **argv)
 {
-     std::vector<double> pb={5,10};
+     std::vector<double> pb={0,14};
     IIRFilter f("butterworth",1,30,pb);
     /* H(z) =\frac{Y(z)}{X(z)}= \frac{\sum_{k=0}^{N}b_k z^{-k}}{\sum_{k=0}^{M}a_k z^{-k}} */
     /* y(n)=b_0x(n)+...b_N x(n-N)-a_1 y(n-1)-...-a_M y(n-M) */
     VideoCapture vid;
-    double amplificationfactor=0.1;
+    double amplificationfactor=0;
 
     vid.open("C:\\Users\\Laurent.PC-LAURENT-VISI\\Documents\\Visual Studio 2013\\AmplificationMouvement\\baby_mp4.mp4");
     if (!vid.isOpened())
@@ -331,7 +347,7 @@ int main(int argc, char **argv)
 		PyramideGaussienne pgAct(m);
 		PyramideLaplacienne plAct(pgAct.get());
 		PyramideRiesz prAct(plAct.get());
-		Pyramide prMotionmaginfiedLap(plAct);
+		PyramideLaplacienne prMotionMagnifiedLap(plAct);
         Mat amplitude;
 		// Valeur de retour 2 pyramides : DiffPaseCos DiffPhaseSin et amplitude
         vector<Pyramide> w=DifferencePhaseAmplitude(plAct,prAct,plPre,prPre);
@@ -349,10 +365,25 @@ int main(int argc, char **argv)
 
             phaseMagnifiedFilteredCos = amplificationfactor*phaseFilterdCos;
             phaseMagnifiedFilteredSin = amplificationfactor*phaseFilterdSin;
-            prMotionmaginfiedLap[i]=PhaseShiftCoefficientRealPart(plAct[i], prAct.getx()[i], prAct.gety()[i], phaseMagnifiedFilteredCos, phaseMagnifiedFilteredSin);
+            prMotionMagnifiedLap[i]=PhaseShiftCoefficientRealPart(plAct[i], prAct.getx()[i], prAct.gety()[i], phaseMagnifiedFilteredCos, phaseMagnifiedFilteredSin);
 
 		}
-        prMotionmaginfiedLap[phaseCos.size()] = plAct[phaseCos.size()];
+        Mat x = prMotionMagnifiedLap.Collpase(pgAct);
+        vector<Mat> sx;
+        split(x,sx);
+        imshow("video",m);
+        vector<double> minVal(3), maxVal(3);
+        minMaxLoc(sx[0],&minVal[0],&maxVal[0]);
+        minMaxLoc(sx[1],&minVal[1],&maxVal[1]);
+        minMaxLoc(sx[2],&minVal[2],&maxVal[2]);
+        maxVal[0] = *max_element(maxVal.begin(),maxVal.end());
+        minVal[0] = *min_element(minVal.begin(),minVal.end());
+        Mat uc;
+        x.convertTo(uc,CV_8U,255/(maxVal[0]-minVal[0]),-255*minVal[0]/(maxVal[0]-minVal[0]));
+        imshow("Laplacian Motion",uc);
+        waitKey(5);
+        cout << vid.get(CAP_PROP_POS_MSEC)<<endl;
+        prMotionMagnifiedLap[phaseCos.size()] = plAct[phaseCos.size()];
         plPre=plAct;
         prPre=prAct;
 
